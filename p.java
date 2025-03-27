36244e673d7cda6f51a888ad8a41ebb1c89d73dd
HonvithNayakaB <honvithnayaka.b@gmail.com>
2025-03-27 14:41:05 +0530

    hi

--- p.java
+++ p.java
@@ -1,5 +1,7 @@
-public class public static void main(String[] args) {
-    public static void main(String []args)
+public class public 
+
+ {
+    public static void main(String [] args)
     {
         System.out.println("hi lalli");
     }
